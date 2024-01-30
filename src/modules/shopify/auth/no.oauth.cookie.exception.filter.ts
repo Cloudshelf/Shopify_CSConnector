025542939f4c9378e3190a29802199383540b37c
Ashley Williams <ashleyww93@gmail.com>
2024-01-30 14:59:46 +0000

    chore: change NoOAuthCookieExceptionFilter

--- src/modules/shopify/auth/no.oauth.cookie.exception.filter.ts
+++ src/modules/shopify/auth/no.oauth.cookie.exception.filter.ts
@@ -1,17 +1,22 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Catch(HttpException)
+@Catch()
 export class NoOAuthCookieExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
+        if ((ctx as any).contextType && (ctx as any).contextType === 'graphql') {
+            //do nothing as we want to see the GQL errors in the manager
+            return;
+        }
+
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
         const status = exception.getStatus();
 
         //If the exception contains "Cannot complete OAuth process" then redirect to the auth page
 
-        if (exception.message.toLowerCase().includes('cannot complete oauth process')) {
+        if (exception.message.toLowerCase().includes('cannot complete oauth process') && request.query.shop) {
             response.redirect(`/shopify/offline/auth?shop=${request.query.shop}`);
             return;
         }
