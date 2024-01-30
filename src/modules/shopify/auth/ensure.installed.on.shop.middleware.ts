bd7381d0a077e39e2bc56840166c9b5082e529d4
Ashley Williams <ashleyww93@gmail.com>
2024-01-30 20:22:09 +0000

    chore: test hardcoded redirect url

--- src/modules/shopify/auth/ensure.installed.on.shop.middleware.ts
+++ src/modules/shopify/auth/ensure.installed.on.shop.middleware.ts
@@ -15,6 +15,12 @@ export class EnsureInstalledOnShopMiddleware implements NestMiddleware {
 
     async use(req: Request, res: Response, next: NextFunction) {
         this.logger.debug(`In EnsureInstalledOnShopMiddleware`);
+
+        if (!this.shopifyApiService.config.isEmbeddedApp) {
+            this.logger.warn('EnsureInstalledOnShopMiddleware should only be used for embedded apps');
+            return;
+        }
+
         const shop = req.query['shop'] as string;
 
         if (!shop) {
@@ -23,16 +29,18 @@ export class EnsureInstalledOnShopMiddleware implements NestMiddleware {
             return;
         }
 
-        this.logger.debug(`Running EnsureInstalledOnShopMiddleware for shop ${shop}`);
+        this.logger.debug(`Checking if shop has installed the app: ${shop}`);
 
         const offlineSessionId = this.shopifyApiService.session.getOfflineId(shop);
-        this.logger.debug(`OfflineSessionId: ${offlineSessionId}`);
         const offlineSession = await this.databaseSessionStorage.loadSession(offlineSessionId);
-        this.logger.debug(`offlineSession: ${JSON.stringify(offlineSession)}`);
 
         if (!offlineSession) {
             this.logger.log(`Session not found for shop ${shop}, redirecting to auth`);
-            return res.redirect(`/shopify/offline/auth?shop=${shop}`);
+            // return res.redirect(`/shopify/offline/auth?shop=${shop}`);
+
+            return res.redirect(
+                `https://redirectutil.cloudshelf.ai/?path=https://development.shopifyconnector.cloudshelf.ai/shopify/offline/auth?shop=${shop}`,
+            );
         }
 
         this.logger.debug(`Session found for shop ${shop}, continuing`);
