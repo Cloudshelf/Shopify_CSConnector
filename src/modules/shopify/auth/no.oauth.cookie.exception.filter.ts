2d6a00cc4f677051ceebf520fb761dc684c50a9d
Ashley Williams <ashleyww93@gmail.com>
2024-01-30 15:11:36 +0000

    Update no.oauth.cookie.exception.filter.ts

--- src/modules/shopify/auth/no.oauth.cookie.exception.filter.ts
+++ src/modules/shopify/auth/no.oauth.cookie.exception.filter.ts
@@ -1,30 +1,40 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 import { Request, Response } from 'express';
 
 @Catch()
 export class NoOAuthCookieExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
+    constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+    catch(exception: unknown, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         if ((ctx as any).contextType && (ctx as any).contextType === 'graphql') {
             //do nothing as we want to see the GQL errors in the manager
             return;
         }
 
+        const { httpAdapter } = this.httpAdapterHost;
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
+        const httpStatus =
+            exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
         //If the exception contains "Cannot complete OAuth process" then redirect to the auth page
-
-        if (exception.message.toLowerCase().includes('cannot complete oauth process') && request.query.shop) {
+        if (
+            (exception as any).message &&
+            (exception as any).message.toLowerCase().includes('cannot complete oauth process') &&
+            request.query.shop
+        ) {
             response.redirect(`/shopify/offline/auth?shop=${request.query.shop}`);
             return;
         }
 
-        response.status(status).json({
-            statusCode: status,
+        const responseBody = {
+            statusCode: httpStatus,
             timestamp: new Date().toISOString(),
-            path: request.url,
-        });
+            path: httpAdapter.getRequestUrl(ctx.getRequest()),
+        };
+
+        httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
     }
 }
