6a95ba92a2cbb852864e87ac798eb0da973ef7a6
Ashley Williams <ashleyww93@gmail.com>
2024-09-02 09:21:54 +0100

    feat: trigger product groups job

---
+++ src/trigger/product-groups/request-product-groups.ts
@@ -0,0 +1,145 @@
+import { BulkOperationStatus } from 'src/graphql/shopifyAdmin/generated/shopifyAdmin';
+import { FlushMode } from '@mikro-orm/core';
+import { logger, task } from '@trigger.dev/sdk/v3';
+import { subDays, subMinutes } from 'date-fns';
+import { BulkOperationType } from 'src/modules/data-ingestion/bulk.operation.type';
+import { BulkOperationUtils } from 'src/modules/data-ingestion/bulk.operation.utils';
+import { RetailerEntity } from 'src/modules/retailer/retailer.entity';
+import { ToolsUtils } from 'src/modules/tools/tools.utils';
+import { AppDataSource } from 'src/trigger/reuseables/orm';
+
+async function buildCollectionTriggerQueryPayload(retailer: RetailerEntity, changesSince?: Date): Promise<string> {
+    const withPublicationStatus = await retailer.supportsWithPublicationStatus();
+    let queryString = '';
+
+    if (changesSince !== undefined) {
+        //we want to build an explicit query string
+        queryString = `updated_at:>'${changesSince.toISOString()}'`;
+
+        queryString = `(query: \"${queryString}\")`;
+    }
+
+    return `{
+        collections${queryString}  {
+          edges {
+            node {
+              id
+              ${withPublicationStatus ? 'publishedOnCurrentPublication' : ''}
+              title
+              handle
+              image {
+                url
+              }
+              storefrontId
+              updatedAt
+              products {
+                edges {
+                  node {
+                    id
+                    featuredImage {
+                          url
+                          id
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`;
+}
+
+export const RequestProductGroupsTask = task({
+    id: 'request-product-groups',
+    queue: {
+        name: `ingestion`,
+        concurrencyLimit: 1,
+    },
+    run: async (payload: { organisationId: string; fullSync: boolean }, { ctx }) => {
+        if (!AppDataSource) {
+            logger.error(`AppDataSource is not set`);
+            throw new Error(`AppDataSource is not set`);
+        }
+
+        const cloudshelfAPI = process.env.CLOUDSHELF_API_URL;
+
+        if (!cloudshelfAPI) {
+            logger.error(`CLOUDSHELF_API_URL is not set`);
+            throw new Error(`CLOUDSHELF_API_URL is not set`);
+        }
+
+        const connectorHost = process.env.SHOPIFY_CONNECTOR_HOST;
+
+        if (!connectorHost) {
+            logger.error(`SHOPIFY_CONNECTOR_HOST is not set`);
+            throw new Error(`SHOPIFY_CONNECTOR_HOST is not set`);
+        }
+
+        const em = AppDataSource.em.fork({
+            flushMode: FlushMode.COMMIT,
+        });
+
+        const retailer = await em.findOne(RetailerEntity, { id: payload.organisationId });
+
+        if (!retailer) {
+            logger.error(`Retailer does not exist for id "${payload.organisationId}"`);
+            throw new Error(`Retailer does not exist for id "${payload.organisationId}"`);
+        }
+
+        logger.info(
+            `Requesting product groups for retailer ${retailer.displayName} (${retailer.id}) (${retailer.domain})`,
+        );
+
+        logger.info(`Checking for running bulk operation`);
+        const currentBulkOperation = await BulkOperationUtils.checkForRunningBulkOperationByRetailer(retailer, {
+            info: logger.info,
+            error: logger.error,
+            warn: logger.warn,
+        });
+
+        if (currentBulkOperation) {
+            if (currentBulkOperation.status === BulkOperationStatus.Running) {
+                logger.warn(
+                    `Shopify is already running a bulk operation for this store. ${JSON.stringify(
+                        currentBulkOperation,
+                    )}`,
+                );
+                //TODO:trigger how to reschedule this task?
+                return;
+            }
+        }
+
+        let changesSince: Date | undefined = undefined;
+        if (!payload.fullSync) {
+            changesSince = retailer.nextPartialSyncRequestTime ?? undefined;
+
+            if (changesSince === undefined) {
+                //If we have never don't a partial sync, lets just get the last days worth of changes...
+                //This is just so we get something.
+                changesSince = subDays(new Date(), 1);
+            }
+
+            retailer.lastPartialSyncRequestTime = changesSince;
+        }
+
+        logger.info(`Building query payload`);
+        const queryPayload = await buildCollectionTriggerQueryPayload(retailer, changesSince);
+
+        logger.info(`Requesting bulk operation with payload`, { queryPayload });
+        await BulkOperationUtils.requestBulkOperation(
+            em,
+            retailer,
+            BulkOperationType.ProductGroupSync,
+            queryPayload,
+            payload.fullSync,
+            {
+                info: logger.info,
+                error: logger.error,
+                warn: logger.warn,
+            },
+        );
+
+        logger.info(`Flushing changes to database`);
+        await em.flush();
+    },
+});
