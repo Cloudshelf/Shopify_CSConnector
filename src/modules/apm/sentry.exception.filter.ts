b09e70a8cbbdd30f93243adafdecc35858271215
Ashley Williams <ashleyww93@gmail.com>
2024-12-31 10:41:36 +0000

    chore: logging in sentry filter

--- src/modules/apm/sentry.exception.filter.ts
+++ src/modules/apm/sentry.exception.filter.ts
@@ -1,21 +1,27 @@
 import { ArgumentsHost, Catch, HttpServer } from '@nestjs/common';
 import { AbstractHttpAdapter, BaseExceptionFilter } from '@nestjs/core';
 import * as Sentry from '@sentry/node';
+import { ExtendedLogger } from '../../utils/ExtendedLogger';
 
 @Catch()
 export class SentryFilter extends BaseExceptionFilter {
+    readonly logger = new ExtendedLogger('SentryFilter');
     handleUnknownError(
         exception: any,
         host: ArgumentsHost,
         applicationRef: HttpServer<any, any> | AbstractHttpAdapter<any, any, any>,
     ): void {
+        this.logger.debug(`In SentryFilter`);
+        this.logger.debug(`sending exception to sentry`, JSON.stringify(exception));
         Sentry.captureException(exception);
 
         if ((host as any).contextType && (host as any).contextType === 'graphql') {
             //do nothing as we want to see the GQL errors in the manager
+            this.logger.debug(`doing nothing so we see the errors passed along to the gql request`);
         } else {
             //do the normal error stuff, so that we return the http error codes if it was a http request
-            super.handleUnknownError(exception, host, applicationRef);
+            this.logger.debug(`calling super`);
+            return super.handleUnknownError(exception, host, applicationRef);
         }
     }
 }
